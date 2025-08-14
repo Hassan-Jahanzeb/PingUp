68dba4867fcdd3ef33847d4c6da44a8db05cdc34
jahanzeb-mughal <hassanjahanzeb8@gmail.com>
2025-08-14 21:49:33 +0500

    ingest edit final

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "pingup-app", signingKey: process.env.INNGEST_SIGNING_KEYs });
+export const inngest = new Inngest({ id: "pingup-app", signingKey: process.env.INNGEST_SIGNING_KEY });
 
 // Ingest function to save user data 
 
