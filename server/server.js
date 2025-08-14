9107999fbd29cb952d909503570c44173127db6d
jahanzeb-mughal <hassanjahanzeb8@gmail.com>
2025-08-14 20:42:10 +0500

    indexjs8888888

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js'
 import { inngest, functions } from "./inngest/index.js"
-import { serve } from "inngest/express";  
+import { serve } from "inngest/express";
 
 
 const app = express()
@@ -14,6 +14,11 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => res.send('Server is running'))
+
+app.get("/api/inngest", (req, res) => {
+    res.status(200).send("OK");
+});
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000
