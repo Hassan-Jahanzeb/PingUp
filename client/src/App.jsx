d4babf6c52a500f67a23cebae688f4f8e7984ef1
jahanzeb-mughal <hassanjahanzeb8@gmail.com>
2025-08-15 19:54:20 +0500

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Feed from './pages/Feed'
@@ -9,12 +9,22 @@ import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
 import Layout from './pages/Layout'
-import { useUser } from '@clerk/clerk-react'
+import { useUser, useAuth } from '@clerk/clerk-react'
 import { Toaster } from 'react-hot-toast'
+
 
 function App() {
   const user = useUser().isSignedIn
   // const user = useUser()
+
+  const { getToken } = useAuth()
+
+  useEffect(() => {
+    if (user) {
+      getToken().then((token) => console.log(token))
+    }
+  }, [user])
+
   return (
     <>
       <Toaster />
